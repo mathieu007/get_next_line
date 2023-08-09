8e517bdb83aed5010a73eaa381399e330a3e2999
mathieu007 <mathieuroy1983@gmail.com>
2023-08-09 07:09:59 -0400

    used many static

--- get_next_line.c
+++ get_next_line.c
@@ -1,106 +1,160 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: math <math@student.42.fr>                  +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/22 08:05:20 by math              #+#    #+#             */
-/*   Updated: 2023/03/08 14:33:19 by math             ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static void	free_line(char **line)
+int	ft_strlen(char *str)
 {
-	if (line && *line)
+	int	len;
+
+	if (!str)
+		return (0);
+	len = 0;
+	while (*str)
 	{
-		free(*line);
-		*line = NULL;
+		len++;
+		str++;
 	}
+	return (len);
 }
 
-static inline int32_t	get_copy_lenght(char *buffer)
+int	get_cpy_len(char *str)
 {
-	int32_t	copy_len;
+	int	len;
 
-	copy_len = 0;
-	while (*buffer)
+	if (!str)
+		return (0);
+	len = 0;
+	while (*str && *str != '\n')
 	{
-		copy_len++;
-		if (*buffer == '\n')
-			break ;
-		buffer++;
+		len++;
+		str++;
 	}
-	return (copy_len);
+	if (*str == '\n')
+		return (len + 1);
+	return (len);
 }
 
-char	*resize_copy_line(char *buffer, char *line, int32_t *line_len)
+char	*cpy_buff_to_line(char *buff, char *line, int buff_len)
 {
-	char	*temp;
-	int32_t	copy_len;
+	char	*old_line;
+	int		i;
+	char	*new;
+	int		len;
 
-	copy_len = get_copy_lenght(&buffer[0]);
-	if (copy_len == 0)
-		return (line);
-	*line_len += copy_len;
-	temp = line;
-	line = malloc(*line_len + 1);
-	if (!line)
-		return (free_line(&temp), NULL);
-	line[*line_len] = '\0';
-	if (temp)
-		ft_strcpy(line, temp);
-	ft_strlcpy(&line[*line_len - copy_len], buffer, copy_len);
-	ft_strcpy(buffer, &buffer[copy_len]);
-	free_line(&temp);
-	return (line);
+	old_line = line;
+	i = 0;
+	len = ft_strlen(old_line) + buff_len;
+	new = malloc(len + 1);
+	new[len] = '\0';
+	while (line && *line)
+		new[i++] = *line++;
+	while (i < len)
+		new[i++] = *buff++;
+	if (old_line)
+		free(old_line);
+	return (new);
 }
 
-char	*get_next_line(int32_t fd)
+static char	*get_line(char *buff, int fd, int buff_len)
 {
-	static char	buffer[BUFFER_SIZE + 1];
-	int32_t		rd;
-	int32_t		line_len;
-	char		*line;
+	static char	*line;
+	char		*cpy;
 
-	if (fd < 0 || BUFFER_SIZE < 0)
+	cpy = line;
+	if (buff_len == 0)
+		return (line = NULL, cpy);
+	line = cpy_buff_to_line(buff, line, buff_len);
+	cpy = line;
+	if (buff[buff_len - 1] == '\n')
+		return (line = NULL, cpy);
+	return (get_next_line(fd));
+}
+
+void	*free_line(char *buff, int fd)
+{
+	char	*line;
+
+	*buff = '\0';
+	line = get_line(buff, fd, 0);
+	if (line)
+		free(line);
+	return (NULL);
+}
+
+int			fail_read = 0;
+
+int	test_read(int fd, char *buff, int read_len)
+{
+	if (fail_read == 1)
+		return (close(fd), -1);
+	return (read(fd, buff, read_len));
+}
+
+char	*get_next_line(int fd)
+{
+	static char	buff[BUFFER_SIZE + 1];
+	static char	*curr;
+	int			rd_len;
+	int			cpy_len;
+
+	if (BUFFER_SIZE <= 0 || fd < 0)
 		return (NULL);
-	rd = 1;
-	line_len = 0;
-	line = NULL;
-	while (rd)
+	if (!curr || !*curr)
 	{
-		if (*buffer)
-			line = resize_copy_line(buffer, line, &line_len);
-		if (line && line[line_len - 1] == '\n')
-			return (line);
-		rd = read(fd, buffer, BUFFER_SIZE);
-		if (rd == -1)
-			return (*buffer = 0, free_line(&line), NULL);
-		if (rd < BUFFER_SIZE)
-			buffer[rd] = '\0';
-		if (rd == 0)
-			return (*buffer = 0, line);
+		curr = buff;
+		rd_len = test_read(fd, curr, BUFFER_SIZE);
+		if (rd_len < 0)
+			return (free_line(curr, fd));
+		curr[rd_len] = '\0';
+		if (rd_len == 0)
+			return (get_line(curr, fd, 0));
 	}
-	return (*buffer = 0, free_line(&line), NULL);
+	cpy_len = get_cpy_len(curr);
+	curr = &curr[cpy_len];
+	return (get_line(&curr[-cpy_len], fd, cpy_len));
 }
 
-// int	main(void)
-// {
-// 	int32_t	fd;
-// 	char	*ret;
+int	main(void)
+{
+	int		fd;
+	char	*line;
 
-// 	fd = open("multiple_line_with_nl", O_RDWR);
-// 	ret = get_next_line(fd);
-// 	printf("line:%s", ret);
-// 	free(ret);
-// 	ret = get_next_line(fd);
-// 	printf("line:%s", ret);
-// 	free(ret);
-// 	ret = get_next_line(fd);
-// 	printf("line:%s", ret);
-// 	free(ret);
-// 	close(fd);
-// }
+	fd = open("file.txt", O_RDONLY);
+	line = get_next_line(fd);
+	printf("%s", line);
+	if (line)
+		free(line);
+	line = get_next_line(fd);
+	printf("%s", line);
+	if (line)
+		free(line);
+	fail_read = 1;
+	line = get_next_line(fd);
+	if (!line)
+	{
+		close(fd);
+		fd = open("file.txt", O_RDONLY);
+	}
+	fail_read = 0;
+	line = get_next_line(fd);
+	printf("%s", line);
+	if (line)
+		free(line);
+	line = get_next_line(fd);
+	printf("%s", line);
+	if (line)
+		free(line);
+	line = get_next_line(fd);
+	printf("%s", line);
+	if (line)
+		free(line);
+	line = get_next_line(fd);
+	printf("%s", line);
+	if (line)
+		free(line);
+	line = get_next_line(fd);
+	printf("%s", line);
+	if (line)
+		free(line);
+	close(fd);
+	return (1);
+}
--- get_next_line.h
+++ get_next_line.h
@@ -1,32 +1,16 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: math <math@student.42.fr>                  +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/08 07:26:48 by math              #+#    #+#             */
-/*   Updated: 2023/03/08 13:24:48 by math             ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #ifndef GET_NEXT_LINE_H
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 10
+#endif
 # define GET_NEXT_LINE_H
 
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 42
-#  define MAX_BATCH 100
-# endif
-# include <fcntl.h>
-# include <limits.h>
-# include <stdbool.h>
-# include <stdint.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <unistd.h>
 
-void	ft_strcpy(char *dst, const char *src);
-void	ft_strlcpy(char *dst, const char *src, int32_t len);
-char	*get_next_line(int32_t fd);
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <fcntl.h>
+
+char	*get_next_line(int fd);
 
 #endif
